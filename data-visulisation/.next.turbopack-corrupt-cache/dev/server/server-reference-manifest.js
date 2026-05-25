93fd8f52561e685ad54ea5e99933d3a6c93ebe2c
DabeetDas <dabeet.das10@gmail.com>
2026-05-25 19:15:02 +0530

    removed useless dev env files

--- data-visulisation/.next.turbopack-corrupt-cache/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"hJfChdtAS322uPSxqOLn4RjvirHCiaIZSPdp6/C3y+4=\"\n}"
--- data-visulisation/.next.turbopack-corrupt-cache/dev/static/chunks/app_page_tsx_11c0ok.._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_0tgko~l._.js","static/chunks/node_modules_recharts_es6_util_020~ik.._.js","static/chunks/node_modules_recharts_es6_state_0h60ds-._.js","static/chunks/node_modules_recharts_es6_component_0wxj~2s._.js","static/chunks/node_modules_recharts_es6_polar_12icw_o._.js","static/chunks/node_modules_recharts_es6_cartesian_0tolgp9._.js","static/chunks/node_modules_recharts_es6_0ok6ovx._.js","static/chunks/node_modules_0767a8~._.js"],
-    source: "dynamic"
-});
